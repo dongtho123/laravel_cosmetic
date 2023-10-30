63b0e3a3109fafa574ef39ae8721a1eafc206cd2
dongtho123 <dongtho0@gmail.com>
2023-10-30 15:06:00 +0700

    first commit

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,148 @@
+
+	<!-- Start Footer Area -->
+	<footer class="footer">
+		<!-- Footer Top -->
+		<div class="footer-top section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer about">
+							<div class="logo">
+								<a href="index.html"><img src="{{asset('backend/img/logo-white.png')}}" alt="#"></a>
+							</div>
+							@php
+								$settings=DB::table('settings')->get();
+							@endphp
+							<p class="text">Chào các bạn , đây là blog của Đông Thọ, được xây dựng như công cụ hỗ trợ giúp mọi người chủ động phân tích, tìm hiểu mỹ phẩm và mua hàng chính hãng bước đầu là dựa trên thành phần của sản phẩm. Đừng quên đồng hành cùng mình để tìm hiểu thông tin về mỹ phẩm nhé !</p>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-2 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer links">
+							<h4>Thông Tin</h4>
+							<ul>
+								<li><a href="{{route('about-us')}}">Trang chủ</a></li>
+								<li><a href="#">Về chúng tôi</a></li>
+								<li><a href="#">Hỗ trợ khách hàng</a></li>
+								<li><a href="{{route('contact')}}">Liên hệ với chúng tôi</a></li>
+							</ul>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-2 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer links">
+							<h4>Danh mục</h4>
+							<ul>
+								<li><a href="#">Hàng mới về</a></li>
+								<li><a href="#">Danh mục sản phẩm</a></li>
+								<li><a href="#">Khuyến mãi</a></li>
+								<li><a href="#">Tin tức</a></li>
+							</ul>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Widget -->
+						<div class="single-footer social">
+							<h4>Kết nối với chúng tôi</h4>
+							<!-- Single Widget -->
+							<div class="contact">
+								<ul>
+									<li><a href="#">Email: dongtho0@gmail.com</a></li>
+								</ul>
+							</div>
+							<!-- End Single Widget -->
+							<div class="sharethis-inline-follow-buttons"></div>
+						</div>
+						<!-- End Single Widget -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Footer Top -->
+		<div class="copyright">
+			<div class="container">
+				<div class="inner">
+					<div class="row">
+						<div class="col-lg-6 col-12">
+							<div class="left">
+							</div>
+						</div>
+						<div class="col-lg-6 col-12">
+							<div class="right">
+								<img src="{{asset('backend/img/payments.png')}}" alt="#">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- /End Footer Area -->
+ 
+	<!-- Jquery -->
+    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery-migrate-3.0.0.js')}}"></script>
+	<script src="{{asset('frontend/js/jquery-ui.min.js')}}"></script>
+	<!-- Popper JS -->
+	<script src="{{asset('frontend/js/popper.min.js')}}"></script>
+	<!-- Bootstrap JS -->
+	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<!-- Color JS -->
+	<script src="{{asset('frontend/js/colors.js')}}"></script>
+	<!-- Slicknav JS -->
+	<script src="{{asset('frontend/js/slicknav.min.js')}}"></script>
+	<!-- Owl Carousel JS -->
+	<script src="{{asset('frontend/js/owl-carousel.js')}}"></script>
+	<!-- Magnific Popup JS -->
+	<script src="{{asset('frontend/js/magnific-popup.js')}}"></script>
+	<!-- Waypoints JS -->
+	<script src="{{asset('frontend/js/waypoints.min.js')}}"></script>
+	<!-- Countdown JS -->
+	<script src="{{asset('frontend/js/finalcountdown.min.js')}}"></script>
+	<!-- Nice Select JS -->
+	<script src="{{asset('frontend/js/nicesellect.js')}}"></script>
+	<!-- Flex Slider JS -->
+	<script src="{{asset('frontend/js/flex-slider.js')}}"></script>
+	<!-- ScrollUp JS -->
+	<script src="{{asset('frontend/js/scrollup.js')}}"></script>
+	<!-- Onepage Nav JS -->
+	<script src="{{asset('frontend/js/onepage-nav.min.js')}}"></script>
+	{{-- Isotope --}}
+	<script src="{{asset('frontend/js/isotope/isotope.pkgd.min.js')}}"></script>
+	<!-- Easing JS -->
+	<script src="{{asset('frontend/js/easing.js')}}"></script>
+
+	<!-- Active JS -->
+	<script src="{{asset('frontend/js/active.js')}}"></script>
+
+	
+	@stack('scripts')
+	<script>
+		setTimeout(function(){
+		  $('.alert').slideUp();
+		},5000);
+		$(function() {
+		// ------------------------------------------------------- //
+		// Multi Level dropdowns
+		// ------------------------------------------------------ //
+			$("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+
+				$(this).siblings().toggleClass("show");
+
+
+				if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+				}
+				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				$('.dropdown-submenu .show').removeClass("show");
+				});
+
+			});
+		});
+	  </script>
